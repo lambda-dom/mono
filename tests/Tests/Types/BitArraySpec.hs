159ec3f75f2db105e3846e3ea2cf5d1950fd2033
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2025-04-19 13:00:35 +0100

    Renaming integral wrappers. (#11)

--- tests/Tests/Types/BitArraySpec.hs
+++ tests/Tests/Types/BitArraySpec.hs
@@ -1,4 +1,4 @@
-module Tests.Types.IntegralBitsSpec (
+module Tests.Types.BitArraySpec (
     -- * Tests.
     spec,
 ) where
@@ -9,7 +9,7 @@ module Tests.Types.IntegralBitsSpec (
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 -- Module to test.
-import Mono.Types.IntegralBits (bitCount, bits, pack)
+import Mono.Types.BitArray (bitCount, bits, pack)
 
 -- Base.
 import Data.Word (Word8, Word16, Word32, Word64)
@@ -17,7 +17,7 @@ import Data.Word (Word8, Word16, Word32, Word64)
 
 -- Main module test driver.
 spec :: Spec
-spec = describe "Mono.Types.IntegralBytes tests" $ do
+spec = describe "Mono.Types.BitArray tests" $ do
     spec_bitCount
     spec_bits
     spec_pack
@@ -55,3 +55,15 @@ spec_pack = describe "bits tests" $ do
         h [True, False, False, False, False, False, False, False] `shouldBe` 1
         h [False, False, False, True, False, False, False, False] `shouldBe` 8
         h [True, True, True, True, True, True, True, True] `shouldBe` 0xff
+
+    it "Success when argument list strictly smaller than bitCount" $ do
+        h [] `shouldBe` 0
+        h [True] `shouldBe` 1
+        h [False, False, False, True] `shouldBe` 8
+
+    it "Success when argument list strictly longer than bitCount" $ do
+        let append = [True, True]
+        h ([False, False, False, False, False, False, False, False] ++ append) `shouldBe` 0
+        h ([True, False, False, False, False, False, False, False] ++ append) `shouldBe` 1
+        h ([False, False, False, True, False, False, False, False] ++ append) `shouldBe` 8
+        h ([True, True, True, True, True, True, True, True] ++ append) `shouldBe` 0xff
