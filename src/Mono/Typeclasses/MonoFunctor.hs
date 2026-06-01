539d3f109825e0a3076ce03e09e7a190dd150ef8
lambda-dom <grodrigues.math@gmail.com>
2026-06-01 12:46:13 +0100

    Binary `MonoFunctor` instances.

--- src/Mono/Typeclasses/MonoFunctor.hs
+++ src/Mono/Typeclasses/MonoFunctor.hs
@@ -12,6 +12,12 @@ module Mono.Typeclasses.MonoFunctor (
 -- Imports.
 -- Base.
 import Data.List.NonEmpty (NonEmpty)
+import Data.Word (Word8)
+
+-- Libraries.
+import qualified Data.ByteString as Bytes (ByteString, map)
+import qualified Data.ByteString.Lazy as LBytes (ByteString, map)
+import qualified Data.ByteString.Short as SBytes (ShortByteString, map)
 
 
 {- | The typeclass for pure monofunctors, the monomorphic version of 'Functor'. -}
@@ -23,6 +29,7 @@ class MonoFunctor a s | s -> a where
 
 
 -- Functor instances.
+-- Base.
 instance MonoFunctor a [a] where
     {-# INLINE monomap #-}
     monomap :: (a -> a) -> [a] -> [a]
@@ -42,3 +49,20 @@ instance MonoFunctor a (Either e a) where
     {-# INLINE monomap #-}
     monomap :: (a -> a) -> Either e a -> Either e a
     monomap = fmap
+
+
+-- Libraries.
+instance MonoFunctor Word8 Bytes.ByteString where
+    {-# INLINE monomap #-}
+    monomap :: (Word8 -> Word8) -> Bytes.ByteString -> Bytes.ByteString
+    monomap = Bytes.map
+
+instance MonoFunctor Word8 LBytes.ByteString where
+    {-# INLINE monomap #-}
+    monomap :: (Word8 -> Word8) -> LBytes.ByteString -> LBytes.ByteString
+    monomap = LBytes.map
+
+instance MonoFunctor Word8 SBytes.ShortByteString where
+    {-# INLINE monomap #-}
+    monomap :: (Word8 -> Word8) -> SBytes.ShortByteString -> SBytes.ShortByteString
+    monomap = SBytes.map
